8945cb09c08bd97e09770dc4745fbf27c46f5b2b
Daniel Tull <dt@danieltull.co.uk>
2011-12-04 13:09:30 +0000

    Add a successful save

--- DCTCoreDataStack Demo/AppDelegate.m
+++ DCTCoreDataStack Demo/AppDelegate.m
@@ -20,10 +20,38 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 	
 	coreDataStack = [[DCTCoreDataStack alloc] initWithModelName:@"DCTCoreDataStack"];
-	
 	NSManagedObjectContext *context = coreDataStack.managedObjectContext;
+	
+	dispatch_queue_t queue = dispatch_get_current_queue();
+	
+	DCTManagedObjectContextSaveCompletionBlock completion = ^(BOOL success) {
+		NSString *mainLabel = [NSString stringWithCString:dispatch_queue_get_label(queue)
+												 encoding:NSUTF8StringEncoding];
+		
+		NSString *currentLabel = [NSString stringWithCString:dispatch_queue_get_label(dispatch_get_current_queue())
+													encoding:NSUTF8StringEncoding];
+		
+		NSLog(@"Completion with %@: main:%@, current:%@", (success?@"success":@"failure"), mainLabel, currentLabel);
+	};
+	
+	DCTManagedObjectContextSaveErrorBlock error = ^(NSError *error) {
+		NSLog(@"%@", [context dct_detailedDescriptionFromValidationError:error]);
+		
+		NSString *mainLabel = [NSString stringWithCString:dispatch_queue_get_label(queue)
+												 encoding:NSUTF8StringEncoding];
+		
+		NSString *currentLabel = [NSString stringWithCString:dispatch_queue_get_label(dispatch_get_current_queue())
+													encoding:NSUTF8StringEncoding];
+		
+		NSLog(@"Error: main:%@, current:%@", mainLabel, currentLabel);
+	};
+	
+	Event *event = [Event insertInManagedObjectContext:context];
+	event.name = @"Some name";
+	[context dct_saveWithErrorHandler:error completionHandler:completion];
+	
 	[Event insertInManagedObjectContext:context];
-	[context dct_save];
+	[context dct_saveWithErrorHandler:error completionHandler:completion];
 	
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
