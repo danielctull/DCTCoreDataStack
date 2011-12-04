e245a07184cba18f4b657b10e5fa261402d66498
Daniel Tull <dt@danieltull.co.uk>
2011-12-04 12:28:24 +0000

    Initial Commit

---
+++ DCTCoreDataStack/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DCTCoreDataStack
+//
+//  Created by Daniel Tull on 04.12.2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
