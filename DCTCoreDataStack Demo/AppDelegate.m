7a81e833df96ad1f945c07b888d4d3466da8e1a2
Daniel Tull <dt@danieltull.co.uk>
2012-01-27 11:35:59 +0000

    Better demo

--- DCTCoreDataStack Demo/AppDelegate.m
+++ DCTCoreDataStack Demo/AppDelegate.m
@@ -22,25 +22,68 @@
 	coreDataStack = [[DCTCoreDataStack alloc] initWithStoreFilename:@"DCTCoreDataStack"];
 	NSManagedObjectContext *context = coreDataStack.managedObjectContext;
 	
+	NSLog(@"%@:%@ ", self, NSStringFromSelector(_cmd));
+	Event *event = [Event insertInManagedObjectContext:context];
+	event.name = @"Some name";
+	NSError *error = nil;
+	BOOL success = [context save:&error];
+	NSLog(@"%@:%@ %@\n %@ \n\n", self, NSStringFromSelector(_cmd), (success?@"success":@"failure"), [context dct_detailedDescriptionFromValidationError:error]);
+	
+	
+	
+	
+	NSManagedObjectContext *threadedContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSPrivateQueueConcurrencyType];
+	threadedContext.parentContext = context;
+	
 	NSString *mainLabel = [NSString stringWithCString:dispatch_queue_get_label(dispatch_get_current_queue())
 											 encoding:NSUTF8StringEncoding];
 	
-	DCTManagedObjectContextSaveCompletionBlock completion = ^(BOOL success, NSError *error) {
+	[threadedContext performBlock:^{
+			
+		Event *event = [Event insertInManagedObjectContext:threadedContext];
+		event.name = @"Some name";		
 		
-		NSString *currentLabel = [NSString stringWithCString:dispatch_queue_get_label(dispatch_get_current_queue())
-													encoding:NSUTF8StringEncoding];
+		NSString *threadLabel = [NSString stringWithCString:dispatch_queue_get_label(dispatch_get_current_queue())
+												   encoding:NSUTF8StringEncoding];
 		
-		NSLog(@"Completion with %@: main:%@, current:%@", (success?@"success":@"failure"), mainLabel, currentLabel);
+		DCTManagedObjectContextSaveCompletionBlock completion = ^(BOOL success, NSError *error) {
+			
+			NSString *currentLabel = [NSString stringWithCString:dispatch_queue_get_label(dispatch_get_current_queue())
+														encoding:NSUTF8StringEncoding];
+			
+			NSLog(@"%@ 1 Completion with %@: main:%@, current:%@", (success?@"success":@"failure"), threadedContext, threadLabel, currentLabel);
+			
+			if (!success) {
+				NSLog(@"%@", [context dct_detailedDescriptionFromValidationError:error]);
+				return;
+			}
+			
+			[context performBlock:^{
+				
+				[context dct_saveWithCompletionHandler:^(BOOL success, NSError *error) {
+					
+					NSString *currentLabel = [NSString stringWithCString:dispatch_queue_get_label(dispatch_get_current_queue())
+																encoding:NSUTF8StringEncoding];
+					
+					NSLog(@"%@ 2 Completion with %@: main:%@, current:%@", (success?@"success":@"failure"), context, mainLabel, currentLabel);
+					
+					if (!success) NSLog(@"%@", [context dct_detailedDescriptionFromValidationError:error]);
+					
+				}];
+			}];
+		};
 		
-		if (!success) NSLog(@"%@", [context dct_detailedDescriptionFromValidationError:error]);
-	};
+		
+		[threadedContext dct_saveWithCompletionHandler:completion];
+		
+		
+		[Event insertInManagedObjectContext:threadedContext];
+		[threadedContext dct_saveWithCompletionHandler:completion];
+	}];
 	
-	Event *event = [Event insertInManagedObjectContext:context];
-	event.name = @"Some name";
-	[context dct_saveWithCompletionHandler:completion];
 	
-	[Event insertInManagedObjectContext:context];
-	[context dct_saveWithCompletionHandler:completion];
+	
+	
 	
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
