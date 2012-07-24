101cf73575c8e1ebed2e88862ab1fc9ef375693b
Daniel Tull <dt@danieltull.co.uk>
2012-07-24 12:16:54 +0100

    Add the standard event table view

---
+++ Demo/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  DCTCoreDataStack
+//
+//  Created by Daniel Tull on 24.07.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <DCTCoreDataStack/DCTCoreDataStack.h>
+#import "Event.h"
+
+@interface ViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ Demo/ViewController.m
@@ -0,0 +1,170 @@
+//
+//  ViewController.m
+//  DCTCoreDataStack
+//
+//  Created by Daniel Tull on 24.07.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <DCTCoreDataStack/DCTCoreDataStack.h>
+
+@interface ViewController ()
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@end
+
+@implementation ViewController {
+	
+}
+
+- (id)initWithStyle:(UITableViewStyle)style {
+    self = [super initWithStyle:style];
+    if (!self) return nil;
+	self.title = @"DCTCoreDataStack";
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	
+	self.navigationItem.leftBarButtonItem = self.editButtonItem;
+	UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
+	self.navigationItem.rightBarButtonItem = addButton;
+}
+
+- (void)insertNewObject:(id)sender {
+	NSManagedObjectContext *context = self.managedObjectContext;
+	Event *event = [Event insertInManagedObjectContext:context];
+    event.date = [NSDate date];
+	event.name = @"Event";
+    
+	[context dct_saveWithCompletionHandler:^(BOOL success, NSError *error) {
+		if (!success)
+			NSLog(@"%@", [context dct_detailedDescriptionFromValidationError:error]);
+	}];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+	return [[self.fetchedResultsController sections] count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	id <NSFetchedResultsSectionInfo> sectionInfo = [self.fetchedResultsController sections][section];
+	return [sectionInfo numberOfObjects];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+		cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+	[self configureCell:cell atIndexPath:indexPath];
+    return cell;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
+        [context deleteObject:[self.fetchedResultsController objectAtIndexPath:indexPath]];
+        
+        NSError *error = nil;
+        if (![context save:&error]) {
+			// Replace this implementation with code to handle the error appropriately.
+			// abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+    }
+}
+
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    return NO;
+}
+
+#pragma mark - Fetched results controller
+
+- (NSFetchedResultsController *)fetchedResultsController {
+	
+    if (!_fetchedResultsController) {
+    
+		NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+		[fetchRequest setEntity:[Event entityInManagedObjectContext:self.managedObjectContext]];
+		[fetchRequest setFetchBatchSize:20];
+		[fetchRequest setSortDescriptors:@[[[NSSortDescriptor alloc] initWithKey:EventAttributes.date ascending:NO]]];
+    
+		self.fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
+																			managedObjectContext:self.managedObjectContext
+																			  sectionNameKeyPath:nil
+																					   cacheName:@"Master"];
+		self.fetchedResultsController.delegate = self;
+    
+		NSError *error = nil;
+		if (![self.fetchedResultsController performFetch:&error]) {
+			NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+			abort();
+		}
+	}
+	
+    return _fetchedResultsController;
+}
+
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
+    [self.tableView beginUpdates];
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
+           atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type {
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject
+       atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type
+      newIndexPath:(NSIndexPath *)newIndexPath {
+    UITableView *tableView = self.tableView;
+    
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            [tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeUpdate:
+            [self configureCell:[tableView cellForRowAtIndexPath:indexPath] atIndexPath:indexPath];
+            break;
+            
+        case NSFetchedResultsChangeMove:
+            [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            [tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    [self.tableView endUpdates];
+}
+
+- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath {
+	Event *event = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    cell.textLabel.text = [event.date description];
+}
+
+@end
