a9f9b0b8511ba7216c17d7f32a866891296952ad
Daniel Tull <dt@danieltull.co.uk>
2012-08-02 15:05:54 +0100

    Remove use of subscripting in the demo

--- Demo/ViewController.m
+++ Demo/ViewController.m
@@ -51,7 +51,7 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-	id <NSFetchedResultsSectionInfo> sectionInfo = [self.fetchedResultsController sections][section];
+	id <NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:section];
 	return [sectionInfo numberOfObjects];
 }
 
