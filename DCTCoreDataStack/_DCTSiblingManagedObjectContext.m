27ca7ef072f46b01d726340a58ca775d6b0d7702
Daniel Tull <dt@danieltull.co.uk>
2012-08-04 10:58:29 +0100

    Manually merge the changes to the original context after saving

--- DCTCoreDataStack/_DCTSiblingManagedObjectContext.m
+++ DCTCoreDataStack/_DCTSiblingManagedObjectContext.m
@@ -15,59 +15,60 @@
 
 - (id)initWithConcurrencyType:(NSManagedObjectContextConcurrencyType)concurrencyType
 		 managedObjectContext:(NSManagedObjectContext *)context {
-	
+
 	self = [super initWithConcurrencyType:concurrencyType];
 	if (!self) return nil;
-	
+
 	_originalContext = context;
-	
+
 	if (_originalContext.parentContext)
 		self.parentContext = _originalContext.parentContext;
-	
+
 	else
 		self.persistentStoreCoordinator = _originalContext.persistentStoreCoordinator;
-	
+
 	return self;
 }
 
 - (void)dct_saveWithCompletionHandler:(void (^)(BOOL, NSError *))completionHandler {
 
-	
-	NSSet *deletedObjects = [self deletedObjects];
-	NSSet *insertedObjects = [self insertedObjects];
-	NSSet *updatedObjects = [self updatedObjects];
-	
-	updatedObjects = [updatedObjects setByAddingObjectsFromSet:insertedObjects];
-	updatedObjects = [updatedObjects setByAddingObjectsFromSet:deletedObjects];
-	
-	NSLog(@"insertedObjects: %@", insertedObjects);
-	NSLog(@"deletedObjects: %@", deletedObjects);
-	NSLog(@"updatedObjects %@", updatedObjects);
-	
-	NSMutableSet *updatedObjectIDs = [[NSMutableSet alloc] initWithCapacity:[updatedObjects count]];
-	[updatedObjects enumerateObjectsUsingBlock:^(NSManagedObject *mo, BOOL *stop) {
-		[updatedObjectIDs addObject:[mo objectID]];
-	}];
-	
+	NSSet *deletedObjectIDs = [self _objectIDsFromManagedObjects:[self deletedObjects]];
+	NSSet *insertedObjectIDs = [self _objectIDsFromManagedObjects:[self insertedObjects]];
+	NSSet *updatedObjectIDs = [self _objectIDsFromManagedObjects:[self updatedObjects]];
+
 	[super dct_saveWithCompletionHandler:^(BOOL success, NSError *error) {
-		
+
 		[_originalContext performBlock:^{
-			
+
+			[deletedObjectIDs enumerateObjectsUsingBlock:^(NSManagedObjectID *objectID, BOOL *stop) {
+				[_originalContext deleteObject:[_originalContext objectWithID:objectID]];
+			}];
+
 			[updatedObjectIDs enumerateObjectsUsingBlock:^(NSManagedObjectID *objectID, BOOL *stop) {
-				NSManagedObject *object = [_originalContext objectWithID:objectID];
-				[_originalContext refreshObject:object mergeChanges:YES];
-				[object willAccessValueForKey:nil];
-				NSLog(@"refreshing: %@", object);
+				NSManagedObject *managedObject = [_originalContext objectWithID:objectID];
+				[managedObject willAccessValueForKey:nil];
+				[_originalContext refreshObject:managedObject mergeChanges:YES];
+
 			}];
-			
-			[_originalContext mergeChangesFromContextDidSaveNotification:nil];
-			
-			[_originalContext dct_saveWithCompletionHandler:^(BOOL success, NSError *error) {
-				if (completionHandler != NULL)
-					completionHandler(success, error);
+
+			[insertedObjectIDs enumerateObjectsUsingBlock:^(NSManagedObjectID *objectID, BOOL *stop) {
+				NSManagedObject *managedObject = [_originalContext objectWithID:objectID];
+				[managedObject willAccessValueForKey:nil];
+				[_originalContext insertObject:managedObject];
 			}];
+
+			if (completionHandler != NULL)
+				completionHandler(success, error);
 		}];
 	}];
+}
+
+- (NSSet *)_objectIDsFromManagedObjects:(NSSet *)managedObjects {
+	NSMutableSet *objectIDs = [[NSMutableSet alloc] initWithCapacity:[managedObjects count]];
+	[managedObjects enumerateObjectsUsingBlock:^(NSManagedObject *managedObject, BOOL *stop) {
+		[objectIDs addObject:[managedObject objectID]];
+	}];
+	return [objectIDs copy];
 }
 
 @end
