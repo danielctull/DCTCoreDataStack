8156fa437e11c5b25e8bbc66334f0fbfc8b55221
Daniel Tull <dt@danieltull.co.uk>
2012-11-06 15:42:09 +0000

    Revert "Use the application's display name if no filename is given"
    This reverts commit 32ec69113342b06952ea6ccbc4e62717d49a1278.

--- DCTCoreDataStack/DCTCoreDataStack.m
+++ DCTCoreDataStack/DCTCoreDataStack.m
@@ -73,11 +73,6 @@ NSString *const DCTCoreDataStackExcludeFromBackupStoreOption = @"DCTCoreDataStac
 
 #pragma mark - Initialization
 
-- (id)init {
-	NSString *applicationName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"];
-	return [self initWithStoreFilename:applicationName];
-}
-
 - (id)initWithStoreURL:(NSURL *)storeURL
 			 storeType:(NSString *)storeType
 		  storeOptions:(NSDictionary *)storeOptions
@@ -86,7 +81,7 @@ NSString *const DCTCoreDataStackExcludeFromBackupStoreOption = @"DCTCoreDataStac
 	
 	NSParameterAssert(storeType);
 	
-	if (!(self = [super init])) return nil;
+	if (!(self = [self init])) return nil;
 	
 	_storeURL = [storeURL copy];
 	_storeType = [storeType copy];
